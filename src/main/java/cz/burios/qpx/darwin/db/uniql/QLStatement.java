7041e03f1735410a4021a8f90601c9d4e95b2c92
burios-gm <buriosca@gmail.com>
2026-09-10 13:54:22 +0200

    Make CRUD statements JSON serializable

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLStatement.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLStatement.java
@@ -1,6 +1,16 @@
 package cz.burios.qpx.darwin.db.uniql;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /** Base type for executable SQL statements. */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = QLSelect.class, name = "select"),
+    @JsonSubTypes.Type(value = QLInsert.class, name = "insert"),
+    @JsonSubTypes.Type(value = QLUpdate.class, name = "update"),
+    @JsonSubTypes.Type(value = QLDelete.class, name = "delete")
+})
 public abstract class QLStatement {
     public abstract void accept(QLVisitor visitor);
 }
