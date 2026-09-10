b14bf26feb0574fc55031131276562c1c0b6f469
burios-gm <buriosca@gmail.com>
2026-09-10 18:59:34 +0200

    Fix QLJoin constructor type compatibility

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLJoin.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLJoin.java
@@ -3,10 +3,10 @@ package cz.burios.qpx.darwin.db.uniql;
 public class QLJoin extends QLExpr {
     public String type = "INNER";
     public QLExpr table;
-    public QLCondition on;
+    public QLExpr on;
 
     public QLJoin() {}
-    public QLJoin(String type, QLExpr table, QLCondition on) {
+    public QLJoin(String type, QLExpr table, QLExpr on) {
         this.type = type;
         this.table = table;
         this.on = on;
