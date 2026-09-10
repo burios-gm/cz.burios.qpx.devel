f4a61f17e062d2b1ba8d2aa5d179fe475270d435
burios-gm <buriosca@gmail.com>
2026-09-10 11:57:43 +0200

    Allow JOIN against tables and subselects

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLJoin.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLJoin.java
@@ -2,8 +2,14 @@ package cz.burios.qpx.darwin.db.uniql;
 
 public class QLJoin extends QLExpr {
     public String type = "INNER";
-    public QLTable table;
+    public QLExpr table;
     public QLCondition on;
+
     public QLJoin() {}
-    public void accept(QLVisitor visitor) { visitor.visit(this); }
+    public QLJoin(String type, QLExpr table, QLCondition on) {
+        this.type = type;
+        this.table = table;
+        this.on = on;
+    }
+    @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
