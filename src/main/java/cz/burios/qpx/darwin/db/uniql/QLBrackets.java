6290168c8550d79cacb0ef5531ed64d3a72d222b
burios-gm <buriosca@gmail.com>
2026-09-10 13:45:30 +0200

    Support aliases on bracketed expressions

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLBrackets.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLBrackets.java
@@ -3,7 +3,9 @@ package cz.burios.qpx.darwin.db.uniql;
 /** Explicit parentheses around any SQL expression. */
 public class QLBrackets extends QLExpr {
     public QLExpr expression;
+    public String alias;
     public QLBrackets() {}
     public QLBrackets(QLExpr expression) { this.expression = expression; }
+    public QLBrackets as(String alias) { this.alias = alias; return this; }
     @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
