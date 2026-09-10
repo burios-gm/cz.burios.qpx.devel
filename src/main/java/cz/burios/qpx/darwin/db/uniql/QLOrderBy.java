aeaa45b7274409d372d8165e2493824a202d6daf
burios-gm <buriosca@gmail.com>
2026-09-10 12:52:17 +0200

    Complete fluent QLOrderBy API

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLOrderBy.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLOrderBy.java
@@ -5,9 +5,24 @@ import java.util.List;
 
 public class QLOrderBy extends QLExpr {
     public List<Item> items = new ArrayList<>();
+
     public static class Item {
         public QLExpr expression;
         public String direction = "ASC";
+        public Item() {}
+        public Item(QLExpr expression, String direction) {
+            this.expression = expression;
+            this.direction = direction;
+        }
     }
-    public void accept(QLVisitor visitor) { visitor.visit(this); }
+
+    public QLOrderBy add(QLExpr expression) { return add(expression, "ASC"); }
+    public QLOrderBy add(QLExpr expression, String direction) {
+        items.add(new Item(expression, direction));
+        return this;
+    }
+    public QLOrderBy asc(QLExpr expression) { return add(expression, "ASC"); }
+    public QLOrderBy desc(QLExpr expression) { return add(expression, "DESC"); }
+
+    @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
