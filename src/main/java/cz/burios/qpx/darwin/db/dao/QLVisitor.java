7d726a0a7eee8634fac4bbf8d458254670ba181a
burios-gm <buriosca@gmail.com>
2026-09-10 10:19:30 +0200

    Add QL visitor

---
+++ src/main/java/cz/burios/qpx/darwin/db/dao/QLVisitor.java
@@ -0,0 +1,15 @@
+package cz.burios.qpx.darwin.db.dao;
+
+public interface QLVisitor {
+    void visit(QLSelect expr);
+    void visit(QLColumn expr);
+    void visit(QLTable expr);
+    void visit(QLJoin expr);
+    void visit(QLWhere expr);
+    void visit(QLCondition expr);
+    void visit(QLValue expr);
+    void visit(QLGroupBy expr);
+    void visit(QLOrderBy expr);
+    void visit(QLLimit expr);
+    void visit(QLOffset expr);
+}
