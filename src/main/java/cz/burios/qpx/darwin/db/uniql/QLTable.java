5a0eb0f9e97c7c8a25c237b726387401786d4fe0
burios-gm <buriosca@gmail.com>
2026-09-10 20:43:01 +0200

    Support QLSchema in QLTable

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLTable.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLTable.java
@@ -5,6 +5,7 @@ public class QLTable extends QLExpr {
     public String alias;
     public QLTable() {}
     public QLTable(String name) { this.name = name; }
+    public QLTable(QLSchema schema) { this.name = schema.asTable().sqlName(); }
     public QLTable as(String alias) { this.alias = alias; return this; }
     public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
