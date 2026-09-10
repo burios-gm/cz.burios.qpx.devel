5d97516c32cccc4b227bcf19f1ecd3df801c5e41
burios-gm <buriosca@gmail.com>
2026-09-10 19:13:59 +0200

    Fix logical condition chaining

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLCondition.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLCondition.java
@@ -9,7 +9,7 @@ public class QLCondition extends QLExpr {
     public QLCondition(QLExpr left, String operator, QLExpr right) {
         this.left = left; this.operator = operator; this.right = right;
     }
-    public QLCondition and(QLCondition other) { return new QLCondition(this, "AND", other); }
-    public QLCondition or(QLCondition other) { return new QLCondition(this, "OR", other); }
+    public QLCondition and(QLCondition other) { return new QLLogical("AND", this, other); }
+    public QLCondition or(QLCondition other) { return new QLLogical("OR", this, other); }
     @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
