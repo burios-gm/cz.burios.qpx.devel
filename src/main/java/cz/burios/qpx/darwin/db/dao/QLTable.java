0b45b67732eb589f88f4f2b3a7e6fea295452f62
burios-gm <buriosca@gmail.com>
2026-09-10 10:19:40 +0200

    Add QL table

---
+++ src/main/java/cz/burios/qpx/darwin/db/dao/QLTable.java
@@ -0,0 +1,18 @@
+package cz.burios.qpx.darwin.db.dao;
+
+import java.util.Objects;
+
+public final class QLTable extends QLExpr {
+    private String name;
+    private String alias;
+
+    public QLTable() {}
+    public QLTable(String name) { this.name = Objects.requireNonNull(name); }
+    public QLTable(String name, String alias) { this(name); this.alias = alias; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getAlias() { return alias; }
+    public void setAlias(String alias) { this.alias = alias; }
+    @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
+}
