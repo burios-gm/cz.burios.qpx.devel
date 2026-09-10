9f88e1fbb7b476fbffa27b0f83d1dfa69aba0525
burios-gm <buriosca@gmail.com>
2026-09-10 14:34:40 +0200

    Add toSQL() to QLDelete

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLDelete.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLDelete.java
@@ -6,5 +6,9 @@ public class QLDelete extends QLStatement {
     public QLDelete() {}
     public QLDelete(QLExpr table) { this.table = table; }
     public QLDelete where(QLExpr expression) { this.where = expression; return this; }
+
+    /** Render this DELETE statement as SQL with '?' parameter placeholders. */
+    public String toSQL() { return QLSql.render(this).sql(); }
+
     @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
