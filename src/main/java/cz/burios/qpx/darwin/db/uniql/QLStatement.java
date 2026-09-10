4f40c0036ba68f3c80e381ea49ccd0145c9f09e6
burios-gm <buriosca@gmail.com>
2026-09-10 13:54:29 +0200

    Unify statement and expression AST base

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLStatement.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLStatement.java
@@ -6,11 +6,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 /** Base type for executable SQL statements. */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
-    @JsonSubTypes.Type(value = QLSelect.class, name = "select"),
     @JsonSubTypes.Type(value = QLInsert.class, name = "insert"),
     @JsonSubTypes.Type(value = QLUpdate.class, name = "update"),
     @JsonSubTypes.Type(value = QLDelete.class, name = "delete")
 })
-public abstract class QLStatement {
-    public abstract void accept(QLVisitor visitor);
+public abstract class QLStatement extends QLExpr {
 }
