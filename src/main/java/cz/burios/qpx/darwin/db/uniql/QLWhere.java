7db10e08cfcfb2abae05ef0371777e4b2fadeb86
burios-gm <buriosca@gmail.com>
2026-09-10 11:57:47 +0200

    Allow nested boolean expressions in WHERE

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLWhere.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLWhere.java
@@ -1,12 +1,17 @@
 package cz.burios.qpx.darwin.db.uniql;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QLWhere extends QLExpr {
-    public List<QLCondition> conditions = new ArrayList<>();
+    public List<QLExpr> expressions = new ArrayList<>();
+
     public QLWhere() {}
-    public QLWhere(List<QLCondition> conditions) { if (conditions != null) this.conditions.addAll(conditions); }
-    public QLWhere add(QLCondition condition) { conditions.add(condition); return this; }
-    public void accept(QLVisitor visitor) { visitor.visit(this); }
+    public QLWhere(QLExpr... expressions) {
+        if (expressions != null) this.expressions.addAll(Arrays.asList(expressions));
+    }
+    public QLWhere add(QLExpr expression) { expressions.add(expression); return this; }
+    public QLWhere and(QLExpr expression) { return add(expression); }
+    @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
