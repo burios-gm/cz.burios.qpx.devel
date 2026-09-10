763c3d53cb1b8095a241616371efe57851419f44
burios-gm <buriosca@gmail.com>
2026-09-10 11:59:12 +0200

    Add composable condition helpers

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLCondition.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLCondition.java
@@ -4,7 +4,12 @@ public class QLCondition extends QLExpr {
     public QLExpr left;
     public String operator;
     public QLExpr right;
+
     public QLCondition() {}
-    public QLCondition(QLExpr left, String operator, QLExpr right) { this.left=left; this.operator=operator; this.right=right; }
-    public void accept(QLVisitor visitor) { visitor.visit(this); }
+    public QLCondition(QLExpr left, String operator, QLExpr right) {
+        this.left = left; this.operator = operator; this.right = right;
+    }
+    public QLCondition and(QLCondition other) { return new QLCondition(this, "AND", other); }
+    public QLCondition or(QLCondition other) { return new QLCondition(this, "OR", other); }
+    @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
