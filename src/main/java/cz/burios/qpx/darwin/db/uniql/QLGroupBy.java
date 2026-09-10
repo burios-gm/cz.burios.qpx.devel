ce5e59c986653717e0c8bec1cb0ba2b17928de7c
burios-gm <buriosca@gmail.com>
2026-09-10 11:57:52 +0200

    Allow arbitrary expressions in GROUP BY

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLGroupBy.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLGroupBy.java
@@ -1,9 +1,15 @@
 package cz.burios.qpx.darwin.db.uniql;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QLGroupBy extends QLExpr {
-    public List<QLColumn> columns = new ArrayList<>();
-    public void accept(QLVisitor visitor) { visitor.visit(this); }
+    public List<QLExpr> expressions = new ArrayList<>();
+    public QLGroupBy() {}
+    public QLGroupBy(QLExpr... expressions) {
+        if (expressions != null) this.expressions.addAll(Arrays.asList(expressions));
+    }
+    public QLGroupBy add(QLExpr expression) { expressions.add(expression); return this; }
+    @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
