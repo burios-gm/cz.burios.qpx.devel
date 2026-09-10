b34439e4e412a6011ffcddc98c0e73a184588ca3
burios-gm <buriosca@gmail.com>
2026-09-10 12:40:16 +0200

    Extend QLVisitor for fluent expression nodes

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLVisitor.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLVisitor.java
@@ -7,9 +7,17 @@ public interface QLVisitor {
     void visit(QLJoin expr);
     void visit(QLWhere expr);
     void visit(QLCondition expr);
+    void visit(QLLogical expr);
     void visit(QLValue expr);
     void visit(QLFunction expr);
+    void visit(QLExpression expr);
+    void visit(QLBrackets expr);
     void visit(QLSubSelect expr);
+    void visit(QLIn expr);
+    void visit(QLBetween expr);
+    void visit(QLIsNull expr);
+    void visit(QLExists expr);
+    void visit(QLCase expr);
     void visit(QLGroupBy expr);
     void visit(QLOrderBy expr);
     void visit(QLLimit expr);
