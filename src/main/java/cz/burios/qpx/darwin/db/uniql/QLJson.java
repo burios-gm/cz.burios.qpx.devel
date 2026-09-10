53fb71845731521a3dc4dd86730522f98e5bec35
burios-gm <buriosca@gmail.com>
2026-09-10 13:54:35 +0200

    Add JSON support for CRUD statements

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLJson.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLJson.java
@@ -7,5 +7,8 @@ public final class QLJson {
     private QLJson() {}
     public static QLSelect fromJson(String json) throws Exception { return MAPPER.readValue(json, QLSelect.class); }
     public static String toJson(QLSelect select) throws Exception { return MAPPER.writeValueAsString(select); }
+    public static QLStatement statementFromJson(String json) throws Exception { return MAPPER.readValue(json, QLStatement.class); }
+    public static String statementToJson(QLStatement statement) throws Exception { return MAPPER.writeValueAsString(statement); }
     public static QLSql.Result toSql(String json) throws Exception { return QLSql.render(fromJson(json)); }
+    public static QLSql.Result statementToSql(String json) throws Exception { return QLSql.render(statementFromJson(json)); }
 }
