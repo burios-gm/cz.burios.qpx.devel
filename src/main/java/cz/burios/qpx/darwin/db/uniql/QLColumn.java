734e28815219faec3735eb29f4c0fa946efe4541
burios-gm <buriosca@gmail.com>
2026-09-10 21:16:12 +0200

    Fix QLColumn qualified-name handling

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLColumn.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLColumn.java
@@ -5,7 +5,7 @@ public class QLColumn extends QLExpr {
     public String alias;
     public QLColumn() {}
     public QLColumn(String name) { this.name = name; }
-    public QLColumn(QLSchema schema) { this.name = schema.asColumn(schema.column).sqlName(); }
+    public QLColumn(QLSchema schema) { this.name = schema.sqlName(); }
     public QLColumn(String name, String alias) { this.name = name; this.alias = alias; }
     public QLColumn as(String alias) { this.alias = alias; return this; }
     public void accept(QLVisitor visitor) { visitor.visit(this); }
