f10cb75d27574560986dc9fe13ad2a1cb7bad5e1
burios-gm <buriosca@gmail.com>
2026-09-10 13:45:26 +0200

    Support aliases on arithmetic expressions

--- src/main/java/cz/burios/qpx/darwin/db/uniql/QLExpression.java
+++ src/main/java/cz/burios/qpx/darwin/db/uniql/QLExpression.java
@@ -5,10 +5,12 @@ public class QLExpression extends QLExpr {
     public QLExpr left;
     public String operator;
     public QLExpr right;
+    public String alias;
 
     public QLExpression() {}
     public QLExpression(QLExpr left, String operator, QLExpr right) {
         this.left = left; this.operator = operator; this.right = right;
     }
+    public QLExpression as(String alias) { this.alias = alias; return this; }
     @Override public void accept(QLVisitor visitor) { visitor.visit(this); }
 }
