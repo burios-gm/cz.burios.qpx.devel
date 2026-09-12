06b578a91c9ea41991444060195d14ddc23dcde6
burios-gm <buriosca@gmail.com>
2026-09-12 19:41:09 +0200

    Test portable column type parameters

--- src/test/java/cz/burios/qpx/darwin/db/metadata/ColumnTypeTest.java
+++ src/test/java/cz/burios/qpx/darwin/db/metadata/ColumnTypeTest.java
@@ -11,23 +11,29 @@ public class ColumnTypeTest {
         MySQLDialect mysql = new MySQLDialect();
         PostgreSQLDialect postgres = new PostgreSQLDialect();
 
-        ColumnMetaData datetime = new ColumnMetaData("CREATED_AT").logicalType(ColumnType.DATETIME).nullable(false);
-        if (!"`CREATED_AT` DATETIME NOT NULL".equals(mysql.columnDefinition(datetime)))
-            throw new AssertionError(mysql.columnDefinition(datetime));
-        if (!"\"CREATED_AT\" TIMESTAMP NOT NULL".equals(postgres.columnDefinition(datetime)))
-            throw new AssertionError(postgres.columnDefinition(datetime));
+        ColumnMetaData datetime = new ColumnMetaData("CREATED_AT").datetime().nullable(false);
+        if (!"`CREATED_AT` DATETIME NOT NULL".equals(mysql.columnDefinition(datetime))) throw new AssertionError(mysql.columnDefinition(datetime));
+        if (!"\"CREATED_AT\" TIMESTAMP NOT NULL".equals(postgres.columnDefinition(datetime))) throw new AssertionError(postgres.columnDefinition(datetime));
 
-        ColumnMetaData string = new ColumnMetaData("NAME").logicalType(ColumnType.STRING).length(120).nullable(false);
+        ColumnMetaData string = new ColumnMetaData("NAME").string(120).nullable(false);
         if (!"`NAME` VARCHAR(120) NOT NULL".equals(mysql.columnDefinition(string))) throw new AssertionError(mysql.columnDefinition(string));
         if (!"\"NAME\" VARCHAR(120) NOT NULL".equals(postgres.columnDefinition(string))) throw new AssertionError(postgres.columnDefinition(string));
+
+        ColumnMetaData decimal = new ColumnMetaData("AMOUNT").decimal(12, 2);
+        if (!"`AMOUNT` DECIMAL(12,2)".equals(mysql.columnDefinition(decimal))) throw new AssertionError(mysql.columnDefinition(decimal));
+        if (!"\"AMOUNT\" NUMERIC(12,2)".equals(postgres.columnDefinition(decimal))) throw new AssertionError(postgres.columnDefinition(decimal));
+
+        ColumnMetaData binary = new ColumnMetaData("DATA").binary(64);
+        if (!"`DATA` VARBINARY(64)".equals(mysql.columnDefinition(binary))) throw new AssertionError(mysql.columnDefinition(binary));
+        if (!"\"DATA\" BYTEA".equals(postgres.columnDefinition(binary))) throw new AssertionError(postgres.columnDefinition(binary));
 
         ColumnMetaData nativeDatetime = new ColumnMetaData("CREATED_AT").type("DATETIME").jdbcType(Types.TIMESTAMP).jdbcTypeName("DATETIME");
         if (mysql.logicalType(nativeDatetime) != ColumnType.DATETIME) throw new AssertionError("MySQL DATETIME mapping failed");
         ColumnMetaData nativeTimestamp = new ColumnMetaData("CREATED_AT").type("timestamp").jdbcType(Types.TIMESTAMP).jdbcTypeName("TIMESTAMP");
         if (mysql.logicalType(nativeTimestamp) != ColumnType.TIMESTAMP) throw new AssertionError("MySQL TIMESTAMP mapping failed");
 
-        ColumnMetaData generated = new ColumnMetaData("UPDATED_AT").logicalType(ColumnType.DATETIME)
-                .nullable(false).generation(ColumnGeneration.INSERT_UPDATE_TIMESTAMP);
+        ColumnMetaData generated = new ColumnMetaData("UPDATED_AT").datetime().nullable(false)
+                .generation(ColumnGeneration.INSERT_UPDATE_TIMESTAMP);
         if (!mysql.columnDefinition(generated).contains("DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
             throw new AssertionError(mysql.columnDefinition(generated));
 
